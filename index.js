667f9bc132ad71a0160dc6ecd329d7180a62e252
mahmoud <mahmouds343@gmail.com>
2022-06-20 10:34:30 +0300

    connected to mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express();
 const path = require('path')
+const mongoose = require('mongoose')
+const dotenv = require("dotenv")
+dotenv.config()
+
+const url = process.env.CONNECTION_URL
+
+mongoose.connect( url, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+} );
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {console.log("Connected to database")});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join)
